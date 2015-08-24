c59cd85bb901d081a6f5c3fe260a9f83bb1233c3
peerless <peerless2012@126.com>
2015-08-24 17:39:51 +0800

    nest scroll view

--- src/com/peerless2012/alldirectionscrollviewdemo/MainActivity.java
+++ src/com/peerless2012/alldirectionscrollviewdemo/MainActivity.java
@@ -1,12 +1,17 @@
 package com.peerless2012.alldirectionscrollviewdemo;
 
 import android.app.Activity;
+import android.graphics.drawable.StateListDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.HorizontalScrollView;
+import android.widget.ScrollView;
 
 public class MainActivity extends Activity {
-
+	private ScrollView scrollView;
+	private HorizontalScrollView horizontalScrollView;
+	StateListDrawable st;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
